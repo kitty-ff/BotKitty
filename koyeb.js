f45869ca2464a714f1510b71478a8ec3d096590c
kitty-ff <carmenmorel1412@gmail.com>
2024-08-30 06:48:40 +0200

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GataNina-Li/GataBot-MD
+RUN git clone https://github.com/kitty-ff/Bot-Kitty
 
-WORKDIR /root/GataBot-MD
+WORKDIR /root/BotKitty-CS
 
-COPY ./root/GataBot-MD
+COPY ./root/BotKitty-CS
 
 RUN yarn install
 
